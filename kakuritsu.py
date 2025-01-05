0d6b95c54d0fc2a33bdb6a7aee4711f03aefae9f
itsumi <115454119+KGithubbb@users.noreply.github.com>
2025-01-06 02:06:48 +0900

    kakuritsu.py を更新

--- kakuritsu.py
+++ kakuritsu.py
@@ -2,15 +2,15 @@ import random
 from random import randint, randrange
 
 I = 0
-Castoria = 0
+luck = 0
 again = "yes"
 while again == "yes":
-    Castoria = 0
+    luck = 0
     I = 0
-    while Castoria != 1000:
+    while luck != 1000:
         I = I + 1
-        Castoria = (randint(1,1000))
-        if Castoria == 1000:
+        luck = (randint(1,1000))
+        if luck == 1000:
             print('1/1000 Youre Luck Is Proved. Kiitos!')
             print(I)
             again = input('Again?: ')
